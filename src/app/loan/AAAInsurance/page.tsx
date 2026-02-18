11ce7e2dda64b5dc466f2b14cd6865d377d62cff
abhijots <abhijot.singh@grazitti.com>
2026-02-18 12:14:14 +0530

    Logo Location Updated

--- src/app/loan/AAAInsurance/page.tsx
+++ src/app/loan/AAAInsurance/page.tsx
@@ -3,7 +3,7 @@ import AgentAppPage from "../../AgentAppPage";
 export default function AAALoanInsurancePage() {
     return <AgentAppPage
         welcomeMessage={"Apply for an Auto Insurance today with SU Insurance (Bundle and Save More)!"}
-        imageUrl={"/su-logo.svg"}
+        imageUrl={"./su-logo.svg"}
         WorkflowImage={"/workflow-auto-insurance-usa.png"}
     />;
 }
