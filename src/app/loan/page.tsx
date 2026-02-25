b53e03c573ee47a33082ce737545cafa89ceaa34
abhijots <abhijot.singh@grazitti.com>
2026-02-25 18:00:57 +0530

    Changed Logo

--- src/app/loan/page.tsx
+++ src/app/loan/page.tsx
@@ -2,8 +2,8 @@ import AgentAppPage from "../AgentAppPage";
 
 export default function LoanPage() {
   return <AgentAppPage 
-  welcomeMessage={"Invest Now in Kotak e-Invest Plus Unit-Linked Insurance Plan"} 
-  imageUrl={"https://upload.wikimedia.org/wikipedia/en/3/39/Kotak_Mahindra_Group_logo.svg"}
+  welcomeMessage={"Welcome to the SU Insurance Voice Agent"} 
+  imageUrl={"./su-logo.svg"}
   WorkflowImage={"/workflow1111.png"}
   />;
 }
